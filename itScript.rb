d4d96614b904f6ac7dcf9adefb032b08f8681778
Ramiro Ivan Flappo <rflappo@sgaroth.(none)>
2014-06-12 09:07:09 -0300

    Agregar titulo + .pdf ext al elemento a descargar; hacer bucle

--- itScript.rb
+++ itScript.rb
@@ -12,7 +12,19 @@ maxDownPerTime = ARGV[0] if ARGV.size > 0
 # debería implemetar el bajar por tags: "it-books.info/tag/programming"
 
 page = Nokogiri::XML(open(startUrl))
-toDownload =  /filepi\.com\/i\/.+http/.match(page.xpath("//td/a/@href").to_s).to_s
-toDownload.slice! "http"
-toDownload = gen + toDownload #Acá tengo el link de descarga.
-exec("wget --referer=#{startUrl} #{toDownload}")
+titleDown = page.xpath("//head/title").to_s
+while (titleDown != "<title>IT eBooks - Free Download - Big Library</title>") do
+	titleDown.slice! "<title>"
+	titleDown.slice! "</title>"
+	titleDown.slice! " - Free Download eBook - pdf"
+	titleDown.gsub!(' ', '.')
+	#Tenemos el título o nombre del archivo.
+	toDownload =  /filepi\.com\/i\/.+http/.match(page.xpath("//td/a/@href").to_s).to_s
+	toDownload.slice! "http"
+	toDownload = gen + toDownload
+	#Acá tengo el link de descarga, con referer y todo.
+	exec("wget --referer=#{startUrl} #{toDownload} --output-document=#{titleDown+".pdf"}")
+end
+
+
+# exec("wget --referer=#{startUrl} #{toDownload} --output-document=#{titleDown+".pdf"}")
