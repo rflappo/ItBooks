fad5e4843876bcf9b7376f28698abdee1f315f98
Ramiro Ivan Flappo <rflappo@sgaroth.(none)>
2014-06-12 02:52:29 -0300

    Descarga de archivo con referer

--- itScript.rb
+++ itScript.rb
@@ -1,12 +1,18 @@
 require 'rubygems' # Ruby -v < 2.0.0
 require 'nokogiri'
 require 'open-uri'
+require 'uri'
 
-startUrl = "http://it-books.info/book/1/"
-downloadUrl = "http://filepi.com/i"
+gen = "http://"
+startUrl = "http://it-ebooks.info/book/1/"
+
 maxDownPerTime = 5
 maxDownPerTime = ARGV[0] if ARGV.size > 0
 
 # debería implemetar el bajar por tags: "it-books.info/tag/programming"
 
-page = Nokogiri::HTML(open(startUrl))
+page = Nokogiri::XML(open(startUrl))
+toDownload =  /filepi\.com\/i\/.+http/.match(page.xpath("//td/a/@href").to_s).to_s
+toDownload.slice! "http"
+toDownload = gen + toDownload #Acá tengo el link de descarga.
+exec("wget --referer=#{startUrl} #{toDownload}")
